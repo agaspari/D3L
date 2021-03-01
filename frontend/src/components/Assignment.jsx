1133b2d4a9008db216331d8f5e5bdc34d67f1bce
colettes <colette11295@gmail.com>
2021-03-01 14:44:20 -0600

    Add assignment detail on click

--- frontend/src/components/Assignment.jsx
+++ frontend/src/components/Assignment.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AssignmentDetail from './AssignmentDetail';
 
 export class Assignment extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-        }
+            showComponent: false,
+        };
+        this.editAssignment = this.editAssignment.bind(this);
+    }
+
+    editAssignment() {
+        this.setState({
+            showComponent: !this.state.showComponent,
+        });
     }
     
     render() {
@@ -16,9 +25,13 @@ export class Assignment extends Component {
         }
 
         return (
-            <div className="Assignment" onClick={editAssignment}>
+            <div className="Assignment" onClick={this.editAssignment}>
                 <header>{title}</header>
                 <p>{content}</p>
+                {this.state.showComponent ?
+                    <AssignmentDetail /> :
+                    null
+                }
             </div>
         )
     }
---
+++ frontend/src/components/AssignmentDetail.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default function AssignmentDetail(props) {
+  return (
+    <div>hello</div>
+  )
+}
