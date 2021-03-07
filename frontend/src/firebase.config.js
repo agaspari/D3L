1ad337506b60e08fececc5cbe12ab9f7182b7b3c
Antonio <26602603+agaspari@users.noreply.github.com>
2021-03-06 22:46:24 -0800

    Cleanup and fixes for login.. hopefully didnt break something

--- frontend/src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyAVBAVzfa0YiKfXlbfnw6ei2sFrCQeR8GA",
-    authDomain: "d3l-project.firebaseapp.com",
-    projectId: "d3l-project",
-    storageBucket: "d3l-project.appspot.com",
-    messagingSenderId: "826861903441",
-    appId: "1:826861903441:web:34112fca903eac0ca52270",
-    measurementId: "G-KFN2KJPEQD"
-};
